52d183fe98b67301f58890d2bb53df47082118ca
Rameswar Bhuyan <rameswar7bhuyan@gmail.com>
2025-01-23 22:16:15 +0000

    02_datatypes.js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 144553
+let accountEmail = "rameswar@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountID = 2 // not allowed
+console.log(accountID);
+accountEmail = "ram@ram.com"
+accountPassword = "11111"
+accountCity = "Bangalore"
+console.table([accountID, accountEmail, accountPassword, accountCity])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3 + 3) // we are using nodejs/ not browser 
+
+console.log(3 + 3);
+
+console.log("rameswar");
+
+
+
+
+let name = "rameswar"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => True/False
+// null
+// undefined => 
+// symbol => unique
+// Object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // Object
