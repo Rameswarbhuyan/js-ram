860fdf19a376b815977c26d6f2eb50bdfcd7b582
Rameswar Bhuyan <rameswar7bhuyan@gmail.com>
2025-01-23 23:07:40 +0000

    01_conversionOperations.js

---
+++ 01_Basics/01_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "rameswar"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number (score)
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn =  "rameswar"
+
+let booleanIsLoggedIn = Boolean (IsLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true; false => 0
+// "" => false
+// "rameswar" => true
+
+let someNumber = 33
+
+let stringNumber = String (stringNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
